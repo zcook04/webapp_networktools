7d5f178c93d25b5e97d9f5025f242be1775a6db9
Zachary Cook <zcook@ZC-MBAIR.local>
2021-07-11 10:23:36 -0400

    configure redux boilerplate to interact with react

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers/rootReducer';
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
---
+++ frontend/src/reducers/rootReducer.jsx
@@ -0,0 +1,11 @@
+const initialState = {
+    devices: {
+
+    }
+}
+
+const rootReducer = (state = initialState, action) => {
+    return state;
+}
+
+export default rootReducer
