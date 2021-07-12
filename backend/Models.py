5f7b5ea5f741249ef5c69128839a6d45ac8cfcb1
Zack <zcook04@gmail.com>
2021-07-12 14:35:47 -0400

    Add device add api function

--- backend/Models.py
+++ backend/Models.py
@@ -20,18 +20,20 @@ class User(db.Model):
 
 class Device(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    owner = db.Column(db.String(120), unique=True, nullable=False)
+    owner = db.Column(db.String(120), nullable=False)
     deviceType = db.Column(db.String(20), nullable=False)
     ipv4 = db.Column(db.String(15), nullable=False)
     username = db.Column(db.String(60), nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    runningConfig = db.Column(db.Text())
+    runningConfig = db.Column(db.Text(), nullable=True)
 
-    def __init__(self, owner, deviceType, username, password):
+    def __init__(self, owner, deviceType, ipv4, username, password, runningConfig):
         self.owner = owner
         self.deviceType = deviceType
+        self.ipv4 = ipv4
         self.username = username
         self.password = password
+        self.runningConfig = runningConfig
 
     def __repr__(self):
         return f'{str(self.owner)}: {self.ipv4}'
--- backend/routes/devices.py
+++ backend/routes/devices.py
@@ -3,10 +3,11 @@ from flask_restful import Resource, reqparse
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 
+from Models import Device, db
+
 # INIT PARENT DEVICE PARSER
 device_parser = reqparse.RequestParser()
 device_parser.add_argument('ipv4', type=str, required=True)
-device_parser.add_argument('owner', type=str, required=True)
 
 
 class MyDevices(Resource):
@@ -28,6 +29,19 @@ class DeviceInfo(Resource):
     @jwt_required()
     def post(self):
         device_parser_copy = device_parser.copy()
+        device_parser_copy.add_argument('password', type=str, required=True)
+        device_parser_copy.add_argument('username', type=str, required=True)
+        device_parser_copy.add_argument('deviceType', type=str, required=True)
+
+        args = device_parser_copy.parse_args()
+        args['owner'] = get_jwt_identity()
+        new_device = Device(
+            username=args['username'], owner=args['owner'], password=args['password'], ipv4=args['ipv4'], deviceType=args['deviceType'], runningConfig='')
+
+        db.session.add(new_device)
+        db.session.commit()
+        return {new_device.ipv4: new_device.owner}, 202
+
         # TEMP RETURN VAL FOR TESTING
         return {'ipv4': '192.168.101.32', 'owner': "zack@zack.com", 'username': 'cisco', 'password': 'cisco', 'deviceType': 'cisco_ios'}, 200
 
