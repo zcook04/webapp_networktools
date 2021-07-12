57c58c6f606c5bfd0f82c90eeb509d9207aa2ed9
Zack <zcook04@gmail.com>
2021-07-12 14:36:39 -0400

    removed unused jwtmanager import

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -6,7 +6,6 @@ import sqlalchemy
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
 
 # INIT BCRYPT
 import os
