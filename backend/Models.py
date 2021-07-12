62167a5b1115d7d3d844481f8962c93437371973
Zack <zcook04@gmail.com>
2021-07-12 14:09:16 -0400

    Add device model

--- backend/Models.py
+++ backend/Models.py
@@ -16,3 +16,22 @@ class User(db.Model):
 
     def __repr__(self):
         return f'{str(self.id)}: {self.email}'
+
+
+class Device(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner = db.Column(db.String(120), unique=True, nullable=False)
+    deviceType = db.Column(db.String(20), nullable=False)
+    ipv4 = db.Column(db.String(15), nullable=False)
+    username = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    runningConfig = db.Column(db.Text())
+
+    def __init__(self, owner, deviceType, username, password):
+        self.owner = owner
+        self.deviceType = deviceType
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'{str(self.owner)}: {self.ipv4}'
