c58f071996ecd8456dc69eb7d744494d100a36e7
Zack <zcook04@gmail.com>
2021-07-12 12:50:49 -0400

    Add link to mydevicecard wrapper

--- backend/routes/devices.py
+++ backend/routes/devices.py
@@ -3,5 +3,5 @@ from flask_restful import Resource
 
 class MyDevices(Resource):
     def get(self):
-        return [{'ipv4': '192.168.101.32', 'username': 'cisco', 'password': 'cisco'},
-                {'ipv4': '192.168.101.33', 'username': 'zack', 'password': 'zack'}]
+        return [{'ipv4': '192.168.101.32', 'username': 'cisco', 'password': 'cisco', 'deviceType': 'cisco_ios'},
+                {'ipv4': '192.168.101.33', 'username': 'zack', 'password': 'zack', 'deviceType': 'cisco_ios'}]
--- frontend/src/components/MyDeviceCard.jsx
+++ frontend/src/components/MyDeviceCard.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 import { connect } from 'react-redux'
 import { getDevices } from '../actions/deviceActions'
 
 import '../css/mydevicecard.css'
 
 function MyDeviceCard(props) {
+    const uri = `/mydevices/${props.auth.user}/${props.ipv4}`
     return (
-        <div className="mydevicecard-card-wrapper">
-            {props.ipv4}
-            {props.deviceType}
-        </div>
+        <Link to={uri} >
+            <div className="mydevicecard-card-wrapper">
+                <p>{props.ipv4}</p>
+                <p>{props.deviceType === "cisco_ios" && "Cisco IOS Device"}</p>
+                <p>{props.hasConfig ? "Config Loaded" : "Intial Config Required"}</p>
+            </div>
+        </Link>
     )
 }
 
