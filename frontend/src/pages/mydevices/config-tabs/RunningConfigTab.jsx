8d3ab36612a0093cb19c4cdb3f1cc33340835d90
Zachary Cook <zcook@ZC-MBAIR.local>
2021-07-13 17:42:29 -0400

    Add get configuration button to configuration tab if config does not exist

--- frontend/src/pages/mydevices/config-tabs/RunningConfigTab.jsx
+++ frontend/src/pages/mydevices/config-tabs/RunningConfigTab.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import '../../../css/tabrunningconfig.css'
+
 function RunningConfigTab(props) {
     let cfg = null
     if(props.devices.ActiveDevice.runningConfig){
         cfg = props.devices.ActiveDevice.runningConfig.split('\\n')
     }
+
+    const handleClick = (e) => {
+
+    }
+
     return (
         <>
             {cfg && cfg.map((line, key) => <div key={key}>{line}</div>)}
+            {!cfg && <>
+            <h3>No Running Configuration Saved</h3>
+            <div className="getConfig-btn" onClick={handleClick}>
+                Get Running Configuration
+            </div> 
+            </>}
         </>
     )
 }
