ed07a6c618c6ee8108562f58fbd7b63853693774
Zack <zcook04@gmail.com>
2021-07-13 11:17:53 -0400

    Update token expiration to 8hours from test value 1min

--- backend/app.py
+++ backend/app.py
@@ -28,7 +28,7 @@ api = Api(app)
 
 # JWT CONFIG
 app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=1)
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=8)
 jwt = JWTManager(app)
 
 # INIT DB
