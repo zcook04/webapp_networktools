6cc95efd8f81ffa39dfd16efff2610c3b764a7f5
Zachary Cook <zcook@ZC-MBAIR.local>
2021-07-14 18:27:14 -0400

    Add hidden content to format tab config output properly

--- frontend/src/pages/mydevices/config-tabs/RoutingTab.jsx
+++ frontend/src/pages/mydevices/config-tabs/RoutingTab.jsx
@@ -100,7 +100,7 @@ function RoutingTab(props) {
         
         </div>}
 
-            {cfg && !props.devices.loading && cfg.map((line, key) => <div key={key}>{line}</div>)}
+            {cfg && !props.devices.loading && cfg.map((line, key) => <div key={key}>{line === "" ? <span className="hidden-content">{key}</span> : line}</div>)}
         </>
     )
 }
